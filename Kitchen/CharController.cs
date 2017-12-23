aca6c5b82fa9dcff6f80cb46a32d08e76cc6460b
Carl <nuclearcarl@hotmail.com>
2017-12-22 16:22:59 -0800

    migrate character movement to charcontrol

--- Kitchen/CharController.cs
+++ Kitchen/CharController.cs
@@ -7,15 +7,21 @@ public class CharController : MonoBehaviour {
 	public float m_Speed = 10f;
 	public float m_TurnSpeed = 100f;
 
+	private Transform thing;
+	private Rigidbody m_Rigidbody;
 	private float m_MovementInputValue;
 	private float m_TurnInputValue;
 	private bool needFreeze = false;
 	CharacterController m_ctrl;
 
+	private Vector3 snapOffSet;
+
 	static Animator anim;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
+		m_Rigidbody = GetComponent<Rigidbody>();
+		snapOffSet = new Vector3(Mathf.Sin((this.transform.rotation.eulerAngles.y * Mathf.PI)/180) * 4f, 5f, Mathf.Cos((this.transform.rotation.eulerAngles.y * Mathf.PI) / 180) * 4f);
 	}
 
 	// Update is called once per frame
@@ -28,6 +34,10 @@ public class CharController : MonoBehaviour {
 		else {
 			anim.SetBool("isWalking", false);
 		}
+
+		if (thing != null) {
+			// Do pick up and drop down
+		}
 	}
 
 	// character move
@@ -36,7 +46,11 @@ public class CharController : MonoBehaviour {
 		m_MovementInputValue = Input.GetAxis("Vertical") * m_Speed * Time.deltaTime;
 		m_TurnInputValue = Input.GetAxis("Horizontal") * m_TurnSpeed * Time.deltaTime;
 
-		transform.Translate(0, 0, m_MovementInputValue);
-		transform.Rotate(0, m_TurnInputValue, 0); // turn speed need optimization
+        Vector3 movement = transform.forward * m_MovementInputValue;
+        Quaternion turnRotation = Quaternion.Euler(0f,m_TurnInputValue,0f);
+		//transform.Translate(0, 0, m_MovementInputValue);
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+		//transform.Rotate(0, m_TurnInputValue, 0);
+		m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
 	}
 }
